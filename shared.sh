3eb2dfa3a9ff851b648d62c85a6547de83db32a5
Anshumaan Reddy <anre4642@colorado.edu>
2025-02-24 15:08:46 -0700

    Revamped buildroot base

--- shared.sh
+++ shared.sh
@@ -3,15 +3,17 @@
 
 # The defconfig from the buildroot directory we use for qemu builds
 QEMU_DEFCONFIG=configs/qemu_aarch64_virt_defconfig
-# Default hardware platform for Raspberry Pi 3B board, especially when building
-RPI3_DEFCONFIG=configs/raspberrypi3_64_defconfig
+# The default hardware platform, when building for hardware
+RPI_DEFCONFIG=configs/raspberrypi3_defconfig
 # The place we store customizations to the qemu configuration
 MODIFIED_QEMU_DEFCONFIG=base_external/configs/aesd_qemu_defconfig
+# The place we store customizations for the project current target
 
-# Place where customizations are stored to RPi configuration
-MODIFIED_RPI3_DEFCONFIG=base_external/configs/aesd_rpi_defconfig
+# The place we store customizations to the rpi configuration
+MODIFIED_RPI_DEFCONFIG=base_external/configs/aesd_rpi_defconfig
+
 
 # The defconfig from the buildroot directory we use for the project
-AESD_DEFAULT_DEFCONFIG=${RPI3_DEFCONFIG}
-AESD_MODIFIED_DEFCONFIG=${MODIFIED_RPI3_DEFCONFIG}
+AESD_DEFAULT_DEFCONFIG=${RPI_DEFCONFIG}
+AESD_MODIFIED_DEFCONFIG=${MODIFIED_RPI_DEFCONFIG}
 AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT=../${AESD_MODIFIED_DEFCONFIG}
