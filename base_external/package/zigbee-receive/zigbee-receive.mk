2a35244d2293fc535e63af5951d983f43794d2b4
Anshumaan Reddy <anre4642@colorado.edu>
2025-02-28 14:26:03 -0700

    Established Zigbee communication on server side (https://github.com/cu-ecen-aeld/final-project-anshubreddy/issues/5)

--- base_external/package/zigbee-receive/zigbee-receive.mk
+++ base_external/package/zigbee-receive/zigbee-receive.mk
@@ -5,7 +5,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-ZIGBEE_RECEIVE_VERSION = '74803ab918904f5c66010f202c87c474664cbf0a'
+ZIGBEE_RECEIVE_VERSION = '723053fa824bc248452b1142462b3fc1276e040c'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -14,12 +14,12 @@ ZIGBEE_RECEIVE_SITE_METHOD = git
 ZIGBEE_RECEIVE_GIT_SUBMODULES = YES
 
 define ZIGBEE_RECEIVE_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/zigbee/zigbee_receive all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/zigbee_server all
 endef
 
 # TODO add the Zigbee receiver utilities/scripts to the installation steps below
-define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/zigbee/zigbee_receive/zigbee_receive $(TARGET_DIR)/usr/bin
+define ZIGBEE_RECEIVE_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/zigbee_server/zigbee_server $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
